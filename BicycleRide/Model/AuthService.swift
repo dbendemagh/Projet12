50b48073488a5860bd61f0b7e40140c0d6dc3c8d
Daniel Bendemagh <daniel.bendemagh@laposte.net>
2019-12-31 18:08:02 +0100

    Add AuthService Class

---
+++ BicycleRide/Model/AuthService.swift
@@ -0,0 +1,35 @@
+//
+//  AuthService.swift
+//  BicycleRide
+//
+//  Created by Daniel BENDEMAGH on 31/12/2019.
+//  Copyright © 2019 Daniel BENDEMAGH. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+public class AuthService {
+    
+    static func getCurrentUser() -> User? {
+        let currentUser = Auth.auth().currentUser
+        
+        if let currentUser = currentUser {
+            return currentUser
+        } else {
+            return nil
+        }
+    }
+    
+    func createUSer(email: String, password: String, completion: @escaping AuthDataResultCallback) {
+        Auth.auth().createUser(withEmail: email, password: password, completion: completion)
+    }
+    
+    func signIn(email: String, password: String, completion: @escaping AuthDataResultCallback) {
+        Auth.auth().signIn(withEmail: email, password: password, completion: completion)
+    }
+    
+    func signOut() throws {
+        try Auth.auth().signOut()
+    }
+}
